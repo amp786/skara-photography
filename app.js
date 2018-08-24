8d10e9e86937422b8b93d7dfc30600a3bb17d797
Ali Morteza Panju <alimorteza.panju@gmail.com>
2018-08-24 14:58:59 -0400

    make it better

--- app.js
+++ app.js
@@ -8,19 +8,21 @@ app.set('view engine', 'ejs');
 var path = require('path');
 app.use (express.static('public'));
 
+var credentials = {
+    user: 'alimorteza.panju@gmail.com',
+    pass: 'gufour123'
+};
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 // Email logic
-app.post('/send-email', function (req, res) {
+app.post('/submit', function (req, res) {
     let transporter = nodeMailer.createTransport({
         host: 'smtp.gmail.com',
         port: 465,
         secure: true,
-        auth: {
-            user: 'alimorteza.panju@gmail.com',
-            pass: 'gufour123'
-        }
+        auth: credentials,
     });
     let mailOptions = {
         from: '"Ali Morteza Panju" <alimorteza.panju@gmail.com>', // sender address
@@ -42,23 +44,12 @@ app.post('/send-email', function (req, res) {
 
 // Routes
 
-// home
-app.get('/', routes.home);
-
-// about
-app.get('/about', routes.about);
-
-// portfolio
-
-// contact
-
-// comingSoon
-app.get('/coming-soon', routes.comingSoon);
-
+// coming-soon-default
+app.get('/', routes.comingSoon);
 
 // notFound
 app.get('*', routes.notFound);
 
-app.listen(process.env.PORT || 5000, function() {
+app.listen(process.env.PORT || 9000, function() {
     console.log('The magic happens on localhost 9000');
 });
